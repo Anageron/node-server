232d30c30906ab32c7969ad46c9ee3cf02b2e4e5
Danil <nikulizadan@mail.ru>
2025-12-26 15:33:35 +0300

    initial

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+
+export const TYPES = {
+    Application: Symbol.for('Application'),
+    ILogger: Symbol.for('ILogger'),
+    IUserController: Symbol.for('IUserController'),
+    LoggerService: Symbol.for('LoggerService'),
+    ExceptionFilter: Symbol.for('ExceptionFilter'),
+}
